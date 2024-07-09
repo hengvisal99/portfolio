1f1d25db1b77d767338a26d7747048d75cd4b015
visalheng <hengvisal350@gmail.com>
2024-07-09 09:04:34 +0700

    update

--- app/layout.tsx
+++ app/layout.tsx
@@ -37,7 +37,7 @@ export default function RootLayout({
         </NextUIProvider> */}
         <Providers>
           <Header />
-          <main>{children}</main>
+          <main  className="overflow-x-hidden">{children}</main>
           <Footer />
           <SpeedInsights />
         </Providers>
